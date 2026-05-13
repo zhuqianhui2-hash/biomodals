e9fc564c63356b2d89914e5ed3115fc2acea9604
y1zhou <zhou.zy.yi@gmail.com>
2026-05-13 14:10:14 +0800

    fix: always require explicitly calling binary when using the runner script
    Previously the helper was constructed such that `biomodals` can be omitted. But we sometimes need to use `modal`, `python`, etc. within the uv environment, hence this change.

--- run.sh
+++ run.sh
@@ -10,12 +10,13 @@ DEFAULT_BIN_NAME='biomodals'
 if [ $# -eq 0 ] || { [ $# -eq 1 ] && [[ "$1" == "${DEFAULT_BIN_NAME}" ]] ; }; then
   uv run --project "${SCRIPT_DIR}" "${DEFAULT_BIN_NAME}" --help
 
-elif [ $# -gt 1 ] && [[ "$1" == "${DEFAULT_BIN_NAME}" ]]; then
-  uv run --project "${SCRIPT_DIR}" "$@"
+# elif [ $# -gt 1 ] && [[ "$1" == "${DEFAULT_BIN_NAME}" ]]; then
+#   uv run --project "${SCRIPT_DIR}" "$@"
 
-elif [ $# -eq 1 ] && [[ "$1" == 'python' ]]; then
-  uv run --project "${SCRIPT_DIR}" python
+# elif [ $# -eq 1 ] && [[ "$1" == 'python' ]]; then
+#   uv run --project "${SCRIPT_DIR}" python
 
 else
-  uv run --project "${SCRIPT_DIR}" "${DEFAULT_BIN_NAME}" "$@"
+  # uv run --project "${SCRIPT_DIR}" "${DEFAULT_BIN_NAME}" "$@"
+  uv run --project "${SCRIPT_DIR}" "$@"
 fi
